c17917ab6a922f0aa0b083d2b70a2c05d3ca341f
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2023-10-01 15:45:57 +0330

    fix: items movies sliders

--- public/images/common/index.ts
+++ public/images/common/index.ts
@@ -1,6 +1,8 @@
 import logoHeader from './Logo.png'
+import animationSpiderMan from './movies/animationSpiderMan.jpg'
 import Annabel from './movies/Annable.jpg'
 import avatar1 from './movies/avatar-2009.webp'
+import batmanBeLeaguered from './movies/Batman Be-Leaguered.jpg'
 import dayShift from './movies/DayShift.jpg'
 import enolaHolmes2 from './movies/EnolaHolmes2.jpg'
 import extraction2 from './movies/extraction2.jpeg'
@@ -10,12 +12,15 @@ import johnWick from './movies/johnWick.jpg'
 import kingsManPic from './movies/kingsman.jpg'
 import lokiThumbnail from './movies/loki-thumbnail.jpg'
 import moneyHeist from './movies/moneyHeist.webp'
+import onePiece from './movies/OnePiece.jpg'
+import theOriginals from './movies/orginals.webp'
 import rikThumbnail from './movies/rik-thumbnail.jpg'
 import spiderManNowayHome from './movies/spidermanNowayHome.jpg'
 import strangerThings from './movies/strangerThings.jpg'
 import supermanSmall from './movies/superManSmall.jpg'
 import theLastOfUsSmall from './movies/theLastOfUsSmall.jpg'
 import thePhantomMenace from './movies/thePhantomMenace.jpeg'
+import venome from './movies/venom.webp'
 import wandaVisionSmall from './movies/wandaVisionSmall.jpg'
 
 export {
@@ -38,4 +43,9 @@ export {
     thePhantomMenace,
     strangerThings,
     moneyHeist,
+    theOriginals,
+    venome,
+    animationSpiderMan,
+    batmanBeLeaguered,
+    onePiece,
 }
--- src/components/templates/DHomeTemplate/resources/components/content/CategoryContentMovies/resources/data/data.ts
+++ src/components/templates/DHomeTemplate/resources/components/content/CategoryContentMovies/resources/data/data.ts
@@ -1,4 +1,18 @@
-import { avatar1, moneyHeist, strangerThings, theLastOfUsSmall, thePhantomMenace } from '@public/images/common'
+import {
+    animationSpiderMan,
+    batmanBeLeaguered,
+    extraction2Thumbnail,
+    interstellar,
+    johnWick,
+    lokiThumbnail,
+    moneyHeist,
+    onePiece,
+    rikThumbnail,
+    strangerThings,
+    theLastOfUsSmall,
+    theOriginals,
+    venome,
+} from '@public/images/common'
 
 import { type TCategoryContentMoviesData } from '../types/types'
 
@@ -6,34 +20,28 @@ export const dataSwipeCategoryContentMovies: TCategoryContentMoviesData[] = [
     {
         data: [
             {
-                name: 'Avatar 1',
-                rate: 1,
-                banner: avatar1,
+                name: 'Extraction 2',
+                rate: 3,
+                banner: extraction2Thumbnail,
                 id: 0,
             },
             {
-                name: 'the Phantom Menace',
-                rate: 1,
-                banner: thePhantomMenace,
+                name: 'Venome',
+                rate: 4,
+                banner: venome,
                 id: 1,
             },
             {
-                name: 'the Phantom Menace',
-                rate: 1,
-                banner: thePhantomMenace,
-                id: 1,
+                name: 'Interstellar',
+                rate: 4,
+                banner: interstellar,
+                id: 2,
             },
             {
-                name: 'the Phantom Menace',
-                rate: 1,
-                banner: thePhantomMenace,
-                id: 1,
-            },
-            {
-                name: 'the Phantom Menace',
-                rate: 1,
-                banner: thePhantomMenace,
-                id: 1,
+                name: 'johnWick',
+                rate: 4,
+                banner: johnWick,
+                id: 3,
             },
         ],
         categoryName: 'محبوب ترین فیلم ها',
@@ -42,25 +50,67 @@ export const dataSwipeCategoryContentMovies: TCategoryContentMoviesData[] = [
     {
         data: [
             {
+                name: 'the Originals',
+                rate: 5,
+                banner: theOriginals,
+                id: 0,
+            },
+            {
+                name: 'loki',
+                rate: 4,
+                banner: lokiThumbnail,
+                id: 1,
+            },
+            {
                 name: 'StrangerThings',
                 rate: 5,
                 banner: strangerThings,
-                id: 0,
+                id: 2,
             },
             {
                 name: 'The last of Us',
                 rate: 5,
                 banner: theLastOfUsSmall,
-                id: 1,
+                id: 3,
             },
             {
                 name: 'Money Heist',
                 rate: 5,
                 banner: moneyHeist,
-                id: 1,
+                id: 4,
             },
         ],
         categoryName: 'پر بازدید ترین سریال ها',
+        id: 1,
+    },
+    {
+        data: [
+            {
+                name: 'Spider-Verse',
+                rate: 3,
+                banner: animationSpiderMan,
+                id: 0,
+            },
+            {
+                name: 'Rick and Morty ',
+                rate: 3,
+                banner: rikThumbnail,
+                id: 1,
+            },
+            {
+                name: 'Batman  lego ',
+                rate: 4,
+                banner: batmanBeLeaguered,
+                id: 2,
+            },
+            {
+                name: 'One Piece ',
+                rate: 4,
+                banner: onePiece,
+                id: 2,
+            },
+        ],
+        categoryName: 'پر بازدید ترین انیمیشن ها',
         id: 1,
     },
 ]
