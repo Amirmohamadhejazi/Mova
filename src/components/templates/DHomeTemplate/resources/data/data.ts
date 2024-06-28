317255055ff4b7c51300888ad33380d118d2c380
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2024-06-28 16:09:30 +0330

    fix: error build

--- src/components/templates/DHomeTemplate/resources/data/data.ts
+++ src/components/templates/DHomeTemplate/resources/data/data.ts
@@ -1,26 +1,24 @@
 import { type TMovieData } from '@molecules/DSwipers/DSwiperMoviesLarge/resources'
 
-import { Annabel, extraction2, johnWick, kingsManPic } from '@public/images/common'
-
 const dataSwiper: TMovieData[] = [
     {
         name: 'Extraction 2',
-        banner: extraction2,
+        banner: 'extraction2',
         id: 0,
     },
     {
         name: 'John Wick',
-        banner: johnWick,
+        banner: 'johnWick',
         id: 1,
     },
     {
         name: 'Kings Man',
-        banner: kingsManPic,
+        banner: 'kingsManPic',
         id: 2,
     },
     {
         name: 'Annabel',
-        banner: Annabel,
+        banner: 'Annabel',
         id: 3,
     },
 ]
